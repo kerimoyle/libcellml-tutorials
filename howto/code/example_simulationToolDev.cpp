eb9c7acb8bb1e44bae2023cb4dc5f396cbd28a79
Keri Moyle <k.moyle@auckland.ac.nz>
2020-09-07 13:31:52 +1200

    Update example_simulationToolDev.cpp

--- howto/code/example_simulationToolDev.cpp
+++ howto/code/example_simulationToolDev.cpp
@@ -20,8 +20,7 @@
 
 int main()
 {
-// STEP 1
-
+    // STEP 1
     // Parse a CellML file into a model.
 
     // Read the file containing the CellML model into a string.
@@ -38,7 +37,7 @@ int main()
     auto model = parser->parseModel(inFileContents.str());
     printErrorsToTerminal(parser);
 
-// STEP 2
+    // STEP 2
     // Resolve the import dependencies (if any) and flatten the model.
 
     if(model->hasUnresolvedImports()) {
